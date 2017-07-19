86ea3b7033bfdfd76d5ad95b06c8159c5021d97f
slavik <slava-ilin-96@mail.ru>
2017-07-19 17:19:59 +0300

    Added new controller

---
+++ BugTracker/BugTracker/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public UserController()
+        {
+        }
+
+        public UserController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
+        public ActionResult CreateClaim(BugTracker.Models.DBClasses.Application app)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            //... 
+            return View();
+        }
+    }
+}
